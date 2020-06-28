9353c5a903b8f938da29c2714bd6457fda019775
Lachlan Haddrell <lachlanhaddrell7@icloud.com>
2020-06-28 15:54:32 +1000

    initial xcode project creation

---
+++ youtube-oneday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-oneday
+//
+//  Created by Lachlan Haddrell on 28/6/20.
+//  Copyright © 2020 haddrelltech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
